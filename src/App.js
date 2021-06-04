69fbb6b81ae7688647e025dd282a38b85be2bcbf
Gautham Krishna K <gkrishnak2001@gmail.com>
2021-06-04 19:35:19 +0530

    Initialized Project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Notes App</h1>
+
+      {
+        // if JWT token
+        //    <Notes />
+        // else
+        //    <Login SignUp />
+      }
+      
+    </div>
+  );
+}
+
+export default App;
