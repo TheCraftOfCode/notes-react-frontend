05742203edb007b1e724d3be83baa92d7930b31f
Manav Patnaik <manavpatnaik@hotmail.com>
2021-06-11 14:25:44 +0530

    Refactored some code

--- src/components/IndividualNotes.js
+++ src/components/IndividualNotes.js
@@ -1,33 +1,41 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
-import IconButton from '@material-ui/core/IconButton';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import EditIcon from '@material-ui/icons/Edit';
-import DeleteIcon from '@material-ui/icons/Delete';
-import "./notes.css"
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardContent from "@material-ui/core/CardContent";
+import IconButton from "@material-ui/core/IconButton";
+import "./notes.css";
+import { DeleteOutlined } from "@material-ui/icons";
+import { Avatar, Typography } from "@material-ui/core";
+import { yellow, green, pink } from "@material-ui/core/colors";
 
 const useStyles = makeStyles((theme) => ({
-
   expand: {
-    transform: 'rotate(0deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
       duration: theme.transitions.duration.shortest,
     }),
   },
   expandOpen: {
-    transform: 'rotate(180deg)',
+    transform: "rotate(180deg)",
   },
-
+  avatar: {
+    backgroundColor: (note) => {
+      if (note.category === "note") {
+        return yellow[700];
+      }
+      if (note.category === "important") {
+        return green[500];
+      }
+      if (note.category === "todos") {
+        return pink[500];
+      }
+    },
+  },
 }));
 
-export default function IndividualNotes(props) {
+export default function IndividualNotes({ note }) {
   const classes = useStyles();
   const [expanded, setExpanded] = React.useState(false);
 
@@ -36,35 +44,28 @@ export default function IndividualNotes(props) {
   };
 
   return (
-    <Card className="notesCard">
-      <CardHeader title={props.each.title}/>
-      <CardContent>
-          {props.each.content.slice(0,100)}
-      </CardContent>
-      <CardActions disableSpacing>
-      <IconButton style={{marginLeft:"auto"}}>
-        <EditIcon />
-      </IconButton>
-      <IconButton>
-        <DeleteIcon />
-      </IconButton>
-      {props.each.content.lenght > 100 ?
-        <IconButton
-          className={clsx(classes.expand, {
-            [classes.expandOpen]: expanded,
-          })}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-           <ExpandMoreIcon /> 
-        </IconButton> : null }
-
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
+    <div>
+      <Card elevation={1}>
+        <CardHeader
+          avatar={
+            <Avatar className={classes.avatar}>
+              {note.category[0].toUpperCase()}
+            </Avatar>
+          }
+          action={
+            <IconButton>
+              <DeleteOutlined />
+            </IconButton>
+          }
+          title={note.title}
+          subheader={note.category}
+        />
         <CardContent>
-          {props.each.content.slice(100)}
+          <Typography variant="body2" color="textSecondary">
+            {note.content.slice(100)}
+          </Typography>
         </CardContent>
-      </Collapse>
-    </Card>
-  );}
+      </Card>
+    </div>
+  );
+}
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,37 +1,41 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import IndividualNotes from "./IndividualNotes"
-import "./notes.css"
-import Navbar from "./Navbar"
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import IndividualNotes from "./IndividualNotes";
+import "./notes.css";
+import Navbar from "./Navbar";
+import { Container } from "@material-ui/core";
 
 function Notes(props) {
+  const [jwtToken, setJwtToken] = useState(localStorage.getItem("token"));
+  const [notes, setNotes] = useState();
+  const [view, setView] = useState();
 
-    const [jwtToken,setJwtToken] = useState(localStorage.getItem("token"));
-    const [notes,setNotes] = useState();
-    const [view,setView] = useState();
+  const getNotes = async () => {
+    const response = await axios.get(
+      "https://xpressnotes.herokuapp.com/api/notes",
+      { headers: { Authorization: "Bearer " + jwtToken } }
+    );
+    setNotes(response.data);
+  };
 
-    const getNotes = () => {
-        axios.get("https://xpressnotes.herokuapp.com/api/notes", {headers : {Authorization:"Bearer "+jwtToken}})
-        .then((response) => setNotes(response.data))
-    }
+  useEffect(() => {
+    getNotes();
+  }, []);
 
-    useEffect(() => {
-        getNotes()
-    }, [])
+  useEffect(() => {
+    setView(
+      notes?.map((note) => <IndividualNotes key={note._id} note={note} />)
+    );
+  }, [notes]);
 
-    useEffect(() => {
-        setView(notes?.map(each => <IndividualNotes each = {each} /> ))
-    }, [notes])
-
-    return (
-        <div style={{width:"100%",height:"100%"}}>
-            <Navbar handlePage={props.handlePage}/>
-            <div className="notes">
-                {view}
-            </div>
-            
-        </div>
-    )
+  return (
+    <div style={{ width: "100%", height: "100%" }}>
+      <Navbar handlePage={props.handlePage} />
+      <Container>
+        <div className="notes">{view}</div>
+      </Container>
+    </div>
+  );
 }
 
-export default Notes
+export default Notes;
