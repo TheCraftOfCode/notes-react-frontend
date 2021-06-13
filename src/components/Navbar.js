796a16d1343af0a1e6b6cdee356f1773401e5e2e
Manav Patnaik <manavpatnaik@hotmail.com>
2021-06-13 13:33:08 +0530

    Added Date display to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./navbar.css";
+import { format } from "date-fns";
 
 function Navbar(props) {
-
-  const email = localStorage.getItem("email")
-  const user = email.substr(0,email.indexOf("@"))
+  const email = localStorage.getItem("email");
+  const user = email.substr(0, email.indexOf("@"));
 
   const handleSignOut = () => {
     localStorage.removeItem("email");
@@ -14,13 +14,19 @@ function Navbar(props) {
 
   return (
     <div className="navWrapper">
-      <div className="navbarText">Hi {user[0].toUpperCase() + user.slice(1)}</div>
+      <div className="navbarText">
+        Hi {user[0].toUpperCase() + user.slice(1)}, Today is the{" "}
+        {format(new Date(), "do, MMMM Y")}
+      </div>
       <div className="navbar">
-      <div onClick={props.handleModal} className="navbarNavItem">Create Note</div>
-      <div onClick={handleSignOut} className="navbarNavItem">Sign Out</div>
+        <div onClick={props.handleModal} className="navbarNavItem">
+          Create Note
+        </div>
+        <div onClick={handleSignOut} className="navbarNavItem">
+          Sign Out
+        </div>
+      </div>
     </div>
-    </div>
-    
   );
 }
 
