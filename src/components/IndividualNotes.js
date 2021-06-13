67f55cedc8bbff6a0068ac37f2c17ce9cf58bbc8
Manav Patnaik <manavpatnaik@hotmail.com>
2021-06-13 14:33:57 +0530

    Edited Note look

--- src/components/IndividualNotes.js
+++ src/components/IndividualNotes.js
@@ -1,33 +1,48 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
-import IconButton from '@material-ui/core/IconButton';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import DeleteIcon from '@material-ui/icons/Delete';
-import "./notes.css"
-import Axios from 'axios';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import clsx from "clsx";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Collapse from "@material-ui/core/Collapse";
+import IconButton from "@material-ui/core/IconButton";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import DeleteIcon from "@material-ui/icons/Delete";
+import "./notes.css";
+import Axios from "axios";
+import { Avatar } from "@material-ui/core";
+import { DeleteOutlined } from "@material-ui/icons";
+import { green, pink, yellow } from "@material-ui/core/colors";
 
 const useStyles = makeStyles((theme) => ({
-
   expand: {
-    transform: 'rotate(0deg)',
-    transition: theme.transitions.create('transform', {
+    transform: "rotate(0deg)",
+    transition: theme.transitions.create("transform", {
       duration: theme.transitions.duration.shortest,
     }),
   },
   expandOpen: {
-    transform: 'rotate(180deg)',
+    transform: "rotate(180deg)",
   },
-
+  avatar: {
+    backgroundColor: (note) => {
+      if (note.category === "note") {
+        return yellow[700];
+      }
+      if (note.category === "important") {
+        return pink[500];
+      }
+      if (note.category === "todo") {
+        return green[500];
+      }
+    },
+  },
 }));
 
 export default function IndividualNotes(props) {
-  const classes = useStyles();
+  const note = props.each;
+  const classes = useStyles(note);
   const [expanded, setExpanded] = React.useState(false);
 
   const handleExpandClick = () => {
@@ -35,48 +50,106 @@ export default function IndividualNotes(props) {
   };
 
   const deleteNote = () => {
-    
-    Axios.delete("https://xpressnotes.herokuapp.com/api/notes/"+props.each._id, 
-    {headers : {Authorization:"Bearer "+props.jwtToken}})
-    .then((response) => {
-      props.getNotes()}) 
-  }
+    Axios.delete(
+      "https://xpressnotes.herokuapp.com/api/notes/" + props.each._id,
+      { headers: { Authorization: "Bearer " + props.jwtToken } }
+    ).then((response) => {
+      props.getNotes();
+    });
+  };
 
   const noteColor = () => {
-    if(props.each.category === "important") return "#f75a5b"
-    if(props.each.category === "note") return "#ffe02c"
-    if(props.each.category === "todo") return "#68eb5c"
-  }
+    if (props.each.category === "important") return "#f75a5b";
+    if (props.each.category === "note") return "#fff096";
+    if (props.each.category === "todo") return "#68eb5c";
+  };
 
   return (
-    <Card className="notesCard" style={{backgroundColor: noteColor()}}>
-      <CardHeader title={props.each.title}/>
-      {!expanded ? 
-        <CardContent>
-            {props.each.content.slice(0,200)} {props.each.content.length > 200 ? <span>...</span> : null }
-        </CardContent> : null }
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          {props.each.content}
-        </CardContent>
-      </Collapse>
-      <CardActions disableSpacing>
-      <IconButton style={{marginLeft:"auto"}} onClick={deleteNote}>
-        <DeleteIcon />
-      </IconButton>
-      {props.each.content.length > 200 ?
-        <IconButton
-          className={clsx(classes.expand, {
-            [classes.expandOpen]: expanded,
-          })}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-           <ExpandMoreIcon /> 
-        </IconButton> : null }
+    <div>
+      {/* <Card className="notesCard" style={{ backgroundColor: noteColor() }}>
+        <CardHeader
+          avatar={
+            <Avatar className={classes.avatar}>
+              {note.category[0].toUpperCase()}
+            </Avatar>
+          }
+          action={
+            <IconButton onClick={() => deleteNote(note.id)}>
+              <DeleteOutlined />
+            </IconButton>
+          }
+          title={note.title}
+          subheader={note.category}
+        />
+        {!expanded ? (
+          <CardContent>
+            {props.each.content.slice(0, 200)}{" "}
+            {props.each.content.length > 200 ? <span>...</span> : null}
+          </CardContent>
+        ) : null}
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>{props.each.content}</CardContent>
+        </Collapse>
+        <CardActions disableSpacing>
+          <IconButton style={{ marginLeft: "auto" }} onClick={deleteNote}>
+            <DeleteIcon />
+          </IconButton>
+          {props.each.content.length > 200 ? (
+            <IconButton
+              className={clsx(classes.expand, {
+                [classes.expandOpen]: expanded,
+              })}
+              onClick={handleExpandClick}
+              aria-expanded={expanded}
+              aria-label="show more"
+            >
+              <ExpandMoreIcon />
+            </IconButton>
+          ) : null}
           {console.log(props)}
-      </CardActions>
-      
-    </Card>
-  );}
+        </CardActions>
+      </Card> */}
+      <div>
+        <Card elevation={1}>
+          <CardHeader
+            avatar={
+              <Avatar className={classes.avatar}>
+                {note.category[0].toUpperCase()}
+              </Avatar>
+            }
+            action={
+              <IconButton onClick={() => deleteNote(note.id)}>
+                <DeleteOutlined />
+              </IconButton>
+            }
+            title={note.title}
+            subheader={note.category}
+          />
+          <CardContent>
+            {!expanded ? (
+              <CardContent>
+                {props.each.content.slice(0, 200)}{" "}
+                {props.each.content.length > 200 ? <span>...</span> : null}
+              </CardContent>
+            ) : null}
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+              <CardContent>{props.each.content}</CardContent>
+            </Collapse>
+            {props.each.content.length > 200 ? (
+              <IconButton
+                className={clsx(classes.expand, {
+                  [classes.expandOpen]: expanded,
+                })}
+                onClick={handleExpandClick}
+                aria-expanded={expanded}
+                aria-label="show more"
+              >
+                <ExpandMoreIcon />
+              </IconButton>
+            ) : null}
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
+}
