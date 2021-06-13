ada2ac71cf21d6f1b773fdeab7e2093f7a6e98ed
Manav Patnaik <manavpatnaik@hotmail.com>
2021-06-13 14:33:35 +0530

    Added masonry to notes

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,54 +1,93 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import IndividualNotes from "./IndividualNotes"
-import "./notes.css"
-import Navbar from "./Navbar"
-import { Container, Modal } from '@material-ui/core';
-import CreateEdit from "./CreateEdit"
-import NoNotes from "./NoNotes"
-import Loader from "./Loader"
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import IndividualNotes from "./IndividualNotes";
+import "./notes.css";
+import Navbar from "./Navbar";
+import { Container, makeStyles, Modal } from "@material-ui/core";
+import CreateEdit from "./CreateEdit";
+import NoNotes from "./NoNotes";
+import Loader from "./Loader";
+import Masonry from "react-masonry-css";
+
+const useStyles = makeStyles((theme) => ({
+  toolbar: theme.mixins.toolbar,
+}));
 
 function Notes(props) {
+  const classes = useStyles();
+  const [jwtToken, setJwtToken] = useState(localStorage.getItem("token"));
+  const [notes, setNotes] = useState();
+  const [view, setView] = useState();
+  const [modal, setModal] = useState(false);
 
-    const [jwtToken,setJwtToken] = useState(localStorage.getItem("token"));
-    const [notes,setNotes] = useState();
-    const [view,setView] = useState();
-    const [modal,setModal] = useState(false);
+  const handleModal = () => {
+    setModal(!modal);
+  };
 
-    const handleModal = () => {setModal(!modal)}
+  const getNotes = () => {
+    axios
+      .get("https://xpressnotes.herokuapp.com/api/notes", {
+        headers: { Authorization: "Bearer " + jwtToken },
+      })
+      .then((response) => setNotes(response.data));
+    console.log(notes);
+  };
 
+  useEffect(() => {
+    getNotes();
+  }, []);
 
-    const getNotes = () => {
-        axios.get("https://xpressnotes.herokuapp.com/api/notes", {headers : {Authorization:"Bearer "+jwtToken}})
-        .then((response) => setNotes(response.data))
-        console.log(notes)
-    }
+  useEffect(() => {
+    setView(
+      notes?.map((each) => (
+        <IndividualNotes each={each} jwtToken={jwtToken} getNotes={getNotes} />
+      ))
+    );
+  }, [notes]);
 
-    useEffect(() => {
-        getNotes()
-    }, [])
+  const breakpoints = {
+    default: 3,
+    1100: 2,
+    700: 1,
+  };
 
-    useEffect(() => {
-        setView(notes?.map(each => <IndividualNotes each={each} jwtToken={jwtToken} getNotes={getNotes} /> ))
-    }, [notes])
-
-    return (
-        <div style={{width:"100%",height:"100%"}}>
-            <Navbar handlePage={props.handlePage} handleModal={handleModal}/>
-            <div className="notes">
-                {notes ? 
-                    notes?.length === 0 ? <NoNotes handleModal={handleModal} /> :  <Container>{view}</Container>
-                    : <Loader />
-                }
-            </div>
-            <Modal 
-                open = {modal}
-                onClose = {() => {setModal(false)}}
-                >
-                <CreateEdit jwtToken={jwtToken} getNotes={getNotes} handleModal={handleModal}/>
-            </Modal>
-        </div>
-    )
+  return (
+    <div style={{ width: "100%", height: "100%" }}>
+      <Navbar handlePage={props.handlePage} handleModal={handleModal} />
+      <div className={classes.toolbar}></div>
+      <div className="notes">
+        {notes ? (
+          notes?.length === 0 ? (
+            <NoNotes handleModal={handleModal} />
+          ) : (
+            <Container>
+              <Masonry
+                breakpointCols={breakpoints}
+                className="my-masonry-grid"
+                columnClassName="my-masonry-grid_column"
+              >
+                {view}
+              </Masonry>
+            </Container>
+          )
+        ) : (
+          <Loader />
+        )}
+      </div>
+      <Modal
+        open={modal}
+        onClose={() => {
+          setModal(false);
+        }}
+      >
+        <CreateEdit
+          jwtToken={jwtToken}
+          getNotes={getNotes}
+          handleModal={handleModal}
+        />
+      </Modal>
+    </div>
+  );
 }
 
-export default Notes
+export default Notes;
